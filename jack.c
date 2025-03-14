e9fb140eaab75b7897f17e423aab4f617be3d919
amoghrathod <amoghrrathod@gmail.com>
2025-03-14 13:27:33 +0530

    Final

--- jack.c
+++ jack.c
@@ -3,78 +3,101 @@
 #include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/list.h>
-#include <linux/sched/signal.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
-#include <linux/sched/task.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("amogh");
-MODULE_DESCRIPTION("Kernel module to create a tree of child processes based on birthdays");
+MODULE_AUTHOR("Amogh");
+MODULE_DESCRIPTION("Kernel module to create a binary tree of child processes based on birthdays");
 
 struct birthday {
     int day;
     int month;
     int year;
-    struct list_head list;  
-    struct task_struct *task;
+    struct birthday *next_level;
+    struct birthday *same_level;
 };
 
-static LIST_HEAD(task_list);
+static struct birthday *root = NULL;
 
-static struct birthday *b = NULL;
-
-static int child_fn(void *data) {
-    b = (struct birthday *)kmalloc(sizeof(struct birthday), GFP_KERNEL);
-    if (!b) {
+static struct birthday* insert(struct birthday *parent, int day, int month, int year) {
+    struct birthday *node = kmalloc(sizeof(struct birthday), GFP_KERNEL);
+    if (!node) {
         printk(KERN_ERR "jackfruit: Memory allocation failed\n");
-        return -ENOMEM;
+        return NULL;
     }
-    b->day = ((int *)data)[0];
-    b->month = ((int *)data)[1];
-    INIT_LIST_HEAD(&b->list);
-    list_add_tail(&b->list, &task_list);
+    node->day = day;
+    node->month = month;
+    node->year = year;
+    node->next_level = NULL;
+    node->same_level = NULL;
+    
+    if (!parent) return node;
+    
+    struct birthday *temp = parent;
+    while (temp->same_level)
+        temp = temp->same_level;
+    temp->same_level = node;
+    
+    return node;
+}
 
-    kthread_stop(current);
-    return 0;
+static void print_levels(struct birthday *node) {
+    int level = 0;
+    while (node) {
+        printk(KERN_INFO "\nLevel %d:\n", level);
+        struct birthday *temp = node;
+        while (temp) {
+            printk(KERN_INFO "%02d/%02d/%04d ", temp->day, temp->month, temp->year);
+            temp = temp->same_level;
+        }
+        node = node->next_level;
+        level++;
+    }
 }
 
 static int __init jack_init(void) {
-    struct birthday *bday;
-    int dates[5][2] = {
-        {15, 1}, {10, 4}, {5, 6}, {12, 9}, {30, 12}
+    int dates[7][3] = {
+        {15, 1, 2000}, {10, 4, 1998}, {5, 6, 1995}, {12, 9, 1992}, {30, 12, 1990}, {22, 3, 1988}, {17, 7, 1985}
     };
     
-    printk(KERN_INFO "JACKFRUIT Kernel Module Inserted: Creating Child Processes\n");
-    INIT_LIST_HEAD(&task_list);
-
-    for (int i = 0; i < 5; i++) {
-        bday = kmalloc(sizeof(struct birthday), GFP_KERNEL);
-        if (!bday) {
-            printk(KERN_ALERT "Memory allocation failed!\n");
-            return -ENOMEM;
+    printk(KERN_INFO "JACKFRUIT Kernel Module Inserted: Creating Level-Based Structure\n");
+    
+    root = insert(NULL, dates[0][0], dates[0][1], dates[0][2]);
+    struct birthday *parent = root;
+    struct birthday *current_level = root;
+    
+    for (int i = 1; i < 7; i++) {
+        if (dates[i][2] > parent->year) {
+            parent->next_level = insert(NULL, dates[i][0], dates[i][1], dates[i][2]);
+            parent = parent->next_level;
+            current_level = parent;
+        } else {
+            insert(current_level, dates[i][0], dates[i][1], dates[i][2]);
         }
-        bday->day = dates[i][0];
-        bday->month = dates[i][1];
-        INIT_LIST_HEAD(&bday->list); 
-        list_add_tail(&bday->list, &task_list);
-        kthread_run(child_fn, (void *)dates[i], "child_thread_%d", i);
     }
-
-    list_for_each_entry(bday, &task_list, list) {  // Traverse the list
-        printk(KERN_INFO "Birthday: %02d/%02d\n", bday->day, bday->month);
-    }
-
+    
+    print_levels(root);
+    printk(KERN_INFO "\n");
     return 0;
 }
 
-static void __exit jack_exit(void) {
-    struct birthday *temp, *next;
-    list_for_each_entry_safe(temp, next, &task_list, list) {
-        list_del(&temp->list);
-        kfree(temp);
+static void free_tree(struct birthday *node) {
+    while (node) {
+        struct birthday *next_level = node->next_level;
+        while (node) {
+            struct birthday *temp = node;
+            node = node->same_level;
+            kfree(temp);
+        }
+        node = next_level;
     }
+}
+
+static void __exit jack_exit(void) {
+    free_tree(root);
     printk(KERN_INFO "Removed all child processes and freed memory\n");
 }
+
 module_init(jack_init);
 module_exit(jack_exit);
