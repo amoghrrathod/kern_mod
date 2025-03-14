2be922c6fd3e1e0c322091c0db6d0422ac77df3d
amoghrathod <amoghrrathod@gmail.com>
2025-03-14 13:42:49 +0530

    Final

--- jack.c
+++ jack.c
@@ -7,22 +7,23 @@
 #include <linux/delay.h>
 #include <linux/mm.h>
 #include <linux/sched.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Amogh");
-MODULE_DESCRIPTION("Kernel module to create a binary tree of child processes based on birthdays and display memory segments");
+MODULE_DESCRIPTION("Kernel module to create a linked list of child processes based on birthdays and display memory segments");
 
 struct birthday {
     int day;
     int month;
     int year;
-    struct birthday *next_level;
-    struct birthday *same_level;
+    int level;
+    struct birthday *next;
 };
 
-static struct birthday *root = NULL;
+static struct birthday *head = NULL;
 
-static struct birthday* insert(struct birthday *parent, int day, int month, int year) {
+static struct birthday* insert(int day, int month, int year, int level) {
     struct birthday *node = kmalloc(sizeof(struct birthday), GFP_KERNEL);
     if (!node) {
         printk(KERN_ERR "jackfruit: Memory allocation failed\n");
@@ -31,42 +32,30 @@ static struct birthday* insert(struct birthday *parent, int day, int month, int 
     node->day = day;
     node->month = month;
     node->year = year;
-    node->next_level = NULL;
-    node->same_level = NULL;
+    node->level = level;
+    node->next = NULL;
     
-    printk(KERN_INFO "Allocated memory at: %px for Birthday: %02d/%02d/%04d\n", node, day, month, year);
+    printk(KERN_INFO "Allocated memory at: %px for Birthday: %02d/%02d/%04d (Level %d)\n", node, day, month, year, level);
     printk(KERN_INFO "Heap Segment: %px\n", node);
     printk(KERN_INFO "Code Segment: %px\n", (void *)jack_init);
     printk(KERN_INFO "Stack Segment: %px\n", (void *)&node);
     
-    if (!parent) return node;
-    
-    struct birthday *temp = parent;
-    while (temp->same_level)
-        temp = temp->same_level;
-    temp->same_level = node;
-    
+    if (!head) {
+        head = node;
+    } else {
+        struct birthday *temp = head;
+        while (temp->next)
+            temp = temp->next;
+        temp->next = node;
+    }
     return node;
 }
 
-static void print_levels(struct birthday *node) {
-    int level = 0;
-    int node_count = 0;
-    struct birthday *current_level = node;
-
-    while (current_level) {
-        struct birthday *temp = current_level;
-        printk(KERN_INFO "\nLevel %d:\n", level);
-        while (temp) {
-            printk(KERN_INFO "Birthday: %02d/%02d/%04d at memory: %px\n", temp->day, temp->month, temp->year, temp);
-            temp = temp->same_level;
-            node_count++;
-        }
-        printk(KERN_INFO "\n");
-        current_level = current_level->next_level;
-        if (node_count == 1 || node_count == 3 || node_count == 7) {
-            level++;
-        }
+static void print_birthdays(void) {
+    struct birthday *temp = head;
+    while (temp) {
+        printk(KERN_INFO "Birthday: %02d/%02d/%04d (Level %d) at memory: %px\n", temp->day, temp->month, temp->year, temp->level, temp);
+        temp = temp->next;
     }
 }
 
@@ -75,42 +64,29 @@ static int __init jack_init(void) {
         {15, 1, 2000}, {10, 4, 1998}, {5, 6, 1995}, {12, 9, 1992}, {30, 12, 1990}, {22, 3, 1988}, {17, 7, 1985}
     };
     
-    printk(KERN_INFO "JACKFRUIT Kernel Module Inserted: Creating Level-Based Structure\n");
+    printk(KERN_INFO "JACKFRUIT Kernel Module Inserted: Creating Process List\n");
     
-    root = insert(NULL, dates[0][0], dates[0][1], dates[0][2]);
-    struct birthday *parent = root;
-    struct birthday *current_level = root;
-    
-    for (int i = 1; i < 7; i++) {
-        if (dates[i][2] > parent->year) {
-            parent->next_level = insert(NULL, dates[i][0], dates[i][1], dates[i][2]);
-            parent = parent->next_level;
-            current_level = parent;
-        } else {
-            insert(current_level, dates[i][0], dates[i][1], dates[i][2]);
-        }
+    for (int i = 0; i < 7; i++) {
+        insert(dates[i][0], dates[i][1], dates[i][2], i);
     }
     
-    print_levels(root);
+    print_birthdays();
     printk(KERN_INFO "\n");
     return 0;
 }
 
-static void free_tree(struct birthday *node) {
-    while (node) {
-        struct birthday *next_level = node->next_level;
-        while (node) {
-            struct birthday *temp = node;
-            printk(KERN_INFO "Freeing memory at: %px\n", temp);
-            node = node->same_level;
-            kfree(temp);
-        }
-        node = next_level;
+static void free_list(void) {
+    struct birthday *temp;
+    while (head) {
+        temp = head;
+        head = head->next;
+        printk(KERN_INFO "Freeing memory at: %px\n", temp);
+        kfree(temp);
     }
 }
 
 static void __exit jack_exit(void) {
-    free_tree(root);
+    free_list();
     printk(KERN_INFO "Removed all child processes and freed memory\n");
 }
 
